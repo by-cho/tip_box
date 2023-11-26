69c5a1fd5eb8fa907e78022faf5f2fd69c3ac72c
by-cho <149221715+by-cho@users.noreply.github.com>
2023-11-25 19:50:35 -0800

    tip screen update

--- tip_dips/lib/screen/tip_screen.dart
+++ tip_dips/lib/screen/tip_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TipScreen extends StatelessWidget {
-  const TipScreen({super.key});
+class TipScreen extends StatefulWidget {
+  TipScreen({super.key});
+  @override
+  State<TipScreen> createState() => _TipScreenState();
+}
+
+class _TipScreenState extends State<TipScreen> {
+  bool is_Calendar = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +28,7 @@ class TipScreen extends StatelessWidget {
                     decoration: BoxDecoration(
                         border: Border.all(
                           width: 2,
+                          color: Color(0xFF035C48),
                         ),
                         borderRadius: BorderRadius.circular(8)),
                     child: Column(
@@ -66,10 +73,57 @@ class TipScreen extends StatelessWidget {
                 ],
               ),
               Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        is_Calendar = false;
+                      });
+                    },
+                    child: Text(
+                      '일일',
+                      style: TextStyle(
+                          color: is_Calendar
+                              ? Colors.grey[400]
+                              : Color(0xFF035C48),
+                          fontWeight:
+                              is_Calendar ? FontWeight.w400 : FontWeight.w700),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        is_Calendar = true;
+                      });
+                    },
+                    child: Text(
+                      '달력',
+                      style: TextStyle(
+                          color: is_Calendar
+                              ? Color(0xFF035C48)
+                              : Colors.grey[400],
+                          fontWeight:
+                              is_Calendar ? FontWeight.w700 : FontWeight.w400),
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
+              is_Calendar ? Text('달력') : Text('일일')
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your FloatingActionButton's onPressed logic here
+        },
+        backgroundColor: Color(0xFF035C48),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
