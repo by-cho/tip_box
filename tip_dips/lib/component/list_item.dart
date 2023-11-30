f5199188a42a099f5dc23d835067741b07627483
by-cho <149221715+by-cho@users.noreply.github.com>
2023-11-29 23:32:41 -1000

    item list

--- tip_dips/lib/component/list_item.dart
+++ tip_dips/lib/component/list_item.dart
@@ -13,32 +13,38 @@ class ListItem extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              Text(itemDate),
-            ],
-          ),
-          Text(
-            '+ \$$tip',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 8),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: Color(0xFF38DEBD),
+            )),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(itemDate),
+              ],
+            ),
+            Text(
+              '+ \$$tip',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
       ),
     );
   }
